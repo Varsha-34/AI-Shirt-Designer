e0816f7c9bd2cfb85bcb67c029b8fd53c47f4acf
Varsha <varshaa4319@gmail.com>
2023-06-20 15:30:30 +0530

    new change

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,8 @@
 const config = {
   development: {
-    backendUrl: "http://localhost:8080/api/v1/dalle",
+    // backendUrl: "http://localhost:8080/api/v1/dalle",
+    backendUrl: "https://varsha-ai-modelling-shirt-designs.onrender.com/",
+
   },
   production: {
     backendUrl: "https://devswag.onrender.com/api/v1/dalle",
